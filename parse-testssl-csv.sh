0f3f6f0f3953cd2dfcf9da9c41d8a00c6b2c2e7a
studebacon <64607989+studebacon@users.noreply.github.com>
2020-06-19 16:01:48 -0500

    First commit

---
+++ parse-testssl-csv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+        echo "parse-testssl-csv.sh <INPUT FILE>"
+        exit 1
+fi
+while IFS= read -r line
+do
+        if [ ! -d SSLParse.log ]
+        then
+                mkdir SSLParse.log
+        else
+                of=${line//\"/}
+                grep "LOW\|MEDIUM\|HIGH\|CRITICAL" $1 | grep $line | cut -d "," -f 2,3 | cut -d "/" -f 2 | sed 's/"//g' | sed 's/,/:/g' | sort | uniq > SSLParse.log/$of
+        fi
+done < <(cat $1 | grep "LOW\|MEDIUM\|HIGH\|CRITICAL" | cut -d "," -f 1 | sort | uniq)
