3c8aeaea1b33a92b2adce02a1d8748df9f603fb9
studebacon <64607989+studebacon@users.noreply.github.com>
2021-01-14 15:07:00 -0600

    Cleanup commands a bit

--- parse-testssl-csv.sh
+++ parse-testssl-csv.sh
@@ -12,6 +12,6 @@ do
                 mkdir SSLParse.log
         else
                 of=${line//\"/}
-                grep "LOW\|MEDIUM\|HIGH\|CRITICAL" $1 | grep $line | cut -d "," -f 2,3 | cut -d "/" -f 2 | sed 's/"//g' | sed 's/,/:/g' | sort | uniq > SSLParse.log/$of
+                grep "LOW\|MEDIUM\|HIGH\|CRITICAL" $1 | grep $line | cut -d "," -f 2,3 | cut -d "/" -f 2 | sed 's/"//g;s/,/:/g' | sort -u > SSLParse.log/$of
         fi
-done < <(cat $1 | grep "LOW\|MEDIUM\|HIGH\|CRITICAL" | cut -d "," -f 1 | sort | uniq)
+done < <(cat $1 | grep "LOW\|MEDIUM\|HIGH\|CRITICAL" | cut -d "," -f 1 | sort -u)
