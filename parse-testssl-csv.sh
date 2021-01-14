a013b1d7e2a2b87d8c5fdb5f07d0485d1c4e401e
studebacon <64607989+studebacon@users.noreply.github.com>
2021-01-14 15:17:03 -0600

    a bit more cleanup using variable

--- parse-testssl-csv.sh
+++ parse-testssl-csv.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+SEVERITY="LOW\|MEDIUM\|HIGH\|CRITICAL"
+
 if [ -z "$1" ]
 then
-        echo "parse-testssl-csv.sh <INPUT FILE>"
-        exit 1
+    echo "parse-testssl-csv.sh <INPUT FILE>"
+    exit 1
 fi
 while IFS= read -r line
 do
-        if [ ! -d SSLParse.log ]
-        then
-                mkdir SSLParse.log
-        else
-                of=${line//\"/}
-                grep "LOW\|MEDIUM\|HIGH\|CRITICAL" $1 | grep $line | cut -d "," -f 2,3 | cut -d "/" -f 2 | sed 's/"//g;s/,/:/g' | sort -u > SSLParse.log/$of
-        fi
-done < <(cat $1 | grep "LOW\|MEDIUM\|HIGH\|CRITICAL" | cut -d "," -f 1 | sort -u)
+    if [ ! -d SSLParse.log ]
+    then
+        mkdir SSLParse.log
+    else
+        of=${line//\"/}
+        grep $SEVERITY $1 | grep $line | cut -d "," -f 2,3 | cut -d "/" -f 2 | sed 's/"//g;s/,/:/g' | sort -u > SSLParse.log/$of
+    fi
+done < <(cat $1 | grep $SEVERITY | cut -d "," -f 1 | sort -u)
